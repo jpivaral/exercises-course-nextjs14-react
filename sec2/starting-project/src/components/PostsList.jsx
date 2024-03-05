892cf737e3aa4421210aabdb607dc7872b1b99db
jpivaral <josepiva15@gmail.com>
2024-03-04 22:17:24 -0600

    Section 2 Cap 24 min 6:32

--- sec2/starting-project/src/components/PostsList.jsx
+++ sec2/starting-project/src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import Post from "./Post";
+import NewPost from './NewPost';
 import styles from './PostsList.module.css';
 
 function PostsList() {
 
     return (
-        <ul className={styles.posts}>
-            <Post author="test" body="test" />
-            <Post author="test2" body="test2" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={styles.posts}>
+                <Post author="test" body="test" />
+                <Post author="test2" body="test2" />
+            </ul>
+        </>
         );
 }
 
