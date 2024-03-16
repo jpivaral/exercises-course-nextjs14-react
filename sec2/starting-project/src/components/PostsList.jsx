055ca949ab2c83b2def8a4bfe0c77ff0afb6fb6b
jpivaral <josepiva15@gmail.com>
2024-03-15 20:55:04 -0600

    Section 2 Cap 47

--- sec2/starting-project/src/components/PostsList.jsx
+++ sec2/starting-project/src/components/PostsList.jsx
@@ -1,28 +1,16 @@
-import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Post from "./Post";
-import NewPost from './NewPost';
-import Modal from './Modal';
 import styles from './PostsList.module.css';
 
-function PostsList({ isPosting, onStopPosting }) {
-
-    const [posts, setPosts] = useState([]);
-
-    function addPostHander(postData) {
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
+function PostsList() {
+    const posts = useLoaderData();
 
     return (
         <>
-            {isPosting && ( 
-                <Modal onClose={onStopPosting} >
-                    <NewPost onCancel={onStopPosting} onAddPost={addPostHander} />
-                </Modal>)
-            } 
             {posts.length > 0 && (            
                 <ul className={styles.posts}>
-                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                    {posts.map((post) => <Post key={post.id} id={post.id} author={post.author} body={post.body} />)}
                 </ul>
                 )}
             {posts.length === 0 && (
