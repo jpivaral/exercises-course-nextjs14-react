161318d7d4ea0e0990e5eb2cf4b88d4dae8c8d68
jpivaral <josepiva15@gmail.com>
2024-03-20 22:29:53 -0600

    Section 3 Cap 112 min 0:00

---
+++ sec3/05-onwards-foodies-starting-project/app/meals/meals-loading.js
@@ -0,0 +1,6 @@
+import style from './meals-loading.module.css';
+
+export default function MealsLoadingPage() {
+    return <p className={style.loading}>Fetching meals... </p>
+
+}
--- sec3/05-onwards-foodies-starting-project/app/meals/page.js
+++ sec3/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,8 +1,38 @@
+import Link from 'next/link';
+import styles from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+import MealsLoadingPage from './meals-loading';
+
+async function Meals() {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+}
+
 export default function MealsPage() {
+
     return (
-        <main> 
-            <h1>Meals Page</h1>
-        </main>
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meals, created <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={styles.cta}>
+                    <Link href='/meals/share'>
+                        Share your favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <Suspense fallback={<MealsLoadingPage />}>
+                    <Meals />
+                </Suspense>
+            </main>
+        </>
         );
   }
   
